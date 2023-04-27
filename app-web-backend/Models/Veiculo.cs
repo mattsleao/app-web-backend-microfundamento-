9d87d433250c869841b5d58d9c04298eb9a68c36
matheusleao <matheusleaobh@gmail.com>
2023-04-27 18:35:24 -0300

    feat:Adicionar o model Veiculo

---
+++ app-web-backend/Models/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app_web_backend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Obrigatório informar a marca!")]
+        public string Marca { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o modelo!")]
+
+        public string Modelo { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar a placa!")]
+
+        public string Placa { get; set; }
+    }
+}
